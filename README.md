4cabc3ab1c5fdac76925f007ee7b7a404a52ed6c
Adam <adam@adam-desktop.(none)>
2008-09-30 18:32:50 -0400

    - First commit of actual codebase

---
+++ README.md
@@ -0,0 +1,41 @@
+# Simple State/Province Select plugin for jQuery
+
+This plugin links a country select tag to a state/province select tag.
+
+The states populate from an array, so no extra requests to the server are required.
+
+Currently supporting these countries: Canada, United States
+
+## Example
+    $(document).ready(function() {
+      $('#country').linkToStates('#state');
+    });
+
+Also see the included example.html
+
+
+## Dependencies
+
+* jQuery (www.jquery.com)
+* jQuery Select Boxes Plugin (www.texotela.co.uk/code/jquery/select)
+
+## License
+Copyright (c) 2008 Adam Daniels
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
---
+++ jquery.stateselect.js
@@ -0,0 +1,101 @@
+/*
+ * Simple State/Province Select plugin for jQuery
+ * 
+ * Example:
+ * $(document).ready(function() {
+ *    $('#country').linkToStates('#state');
+ *  });
+ *
+ * Copyright (c) 2008 Adam Daniels
+ * Licensed under the MIT License *
+ */
+;(function($) {
+  var canadian_provinces = {
+		'Ontario' : 'Ontario',
+		'Quebec' : 'Quebec',
+		'Manitoba' : 'Manitoba',
+		'Alberta' : 'Alberta',
+		'New Foundland and Labrador' : 'New Foundland and Labrador',
+		'Nova Scotia' : 'Nova Scotia',
+		'Prince Edward Island' : 'Prince Edward Island',
+		'Saskatchewan' : 'Saskatchewan',
+		'Nunavut' : 'Nunavut',
+		'British Columbia' : 'British Columbia',
+		'New Brunswick' : 'New Brunswick',
+		'Yukon' : 'Yukon',
+		'Northwest Territories' : 'Northwest Territories'
+	}
+
+	var us_states = {
+		'Alabama' : 'Alabama',
+		'Alaska' : 'Alaska',
+		'Arizona' : 'Arizona',
+		'Arkansas' : 'Arkansas',
+		'California' : 'California',
+		'Colorado' : 'Colorado',
+		'Connecticut' : 'Connecticut',
+		'Delaware' : 'Delaware',							
+		'Florida' : 'Florida',
+		'Georgia' : 'Georgia',
+		'Hawaii' : 'Hawaii',
+		'Idaho' : 'Idaho',
+		'Illinois' : 'Illinois',
+		'Indiana' : 'Indiana',
+		'Iowa' : 'Iowa',
+		'Kansas' : 'Kansas',
+		'Kentucky' : 'Kentucky',
+		'Louisiana' : 'Louisiana',
+		'Maine' : 'Maine',
+		'Maryland' : 'Maryland',
+		'Massachusetts' : 'Massachusetts',
+		'Michigan' : 'Michigan',
+		'Minnesota' : 'Minnesota',
+		'Mississippi' : 'Mississippi',
+		'Missouri' : 'Missouri',
+		'Montana' : 'Montana',
+		'Nebraska' : 'Nebraska',
+		'Nevada' : 'Nevada',
+		'New Hampshire' : 'New Hampshire',
+		'New Jersey' : 'New Jersey',							
+		'New Mexico' : 'New Mexico',
+		'New York' : 'New York',
+		'North Carolina' : 'North Carolina',
+		'North Dakota' : 'North Dakota',
+		'Ohio' : 'Ohio',
+		'Oklahoma' : 'Oklahoma',
+		'Oregon' : 'Oregon',
+		'Pennsylvania' : 'Pennsylvania',
+		'Rhode Island' : 'Rhode Island',
+		'South Carolina' : 'South Carolina',
+		'South Dakota' : 'South Dakota',
+		'Tennessee' : 'Tennessee',
+		'Texas' : 'Texas',
+		'Utah' : 'Utah',
+		'Vermont' : 'Vermont',
+		'Virginia' : 'Virginia',
+		'Washington' : 'Washington',
+		'West Virginia' : 'West Virginia',
+		'Wisconsin' : 'Wisconsin',
+		'Wyoming' : 'Wyoming'
+	}
+  
+  $.fn.extend({
+		linkToStates: function(state_select_id) {
+      $(this).change(function() {
+        var country = $(this).attr('value');
+        $(state_select_id).removeOption(/.*/);
+        switch (country) {
+          case 'Canada':
+            $(state_select_id).addOption(canadian_provinces, false);
+            break;
+          case 'United States':
+            $(state_select_id).addOption(us_states, false);
+            break;
+          default:
+            $(state_select_id).addOption({ '' : 'Please select a Country'}, false);
+            break;
+        }
+      });
+    }
+  });
+})(jQuery);
